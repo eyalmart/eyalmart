68d9c38b60bd7fdf583ecb41860bd6c1bc0c5f3f
eyalmart <eyal.connect@gmail.com>
2025-12-03 09:37:34 +0530

    Update service-worker.js 031225 1

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,44 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker Installed");
+const CACHE_NAME = "eyalmart-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", () => {
-  // Allows your iframe (Apps Script web app) to work normally
+// Activate - remove old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch handler
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() => caches.match("./index.html"))
+      );
+    })
+  );
 });
